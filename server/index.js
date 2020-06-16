af0d8edf9f2c15cf0f998ae4a111ba1d9593444b
landon tipantiza <landontipantiza@gmail.com>
2020-06-16 04:10:21 +0000

    initial commit set up nav bar and header partially

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
@@ -21,7 +20,6 @@ const db = require("./lib/in-memory-db");
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
-
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
@@ -32,3 +30,4 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
