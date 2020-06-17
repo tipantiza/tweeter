c47a959447d15c389e96d06f89ea45c09885007a
landon tipantiza <landontipantiza@gmail.com>
2020-06-17 01:24:07 +0000

    counter updates properly

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-  console.log("hi");
-  // --- our code goes here --
+  // const $
+  $('textarea').keyup(function (){
+    const $input = $(this);
+    const valueLength = $input.val().length;
+    
+    let limit = 140
+    const finalCount = (limit - valueLength);
+    let $counter = $input.parents().find(".counter");
+
+    if (finalCount < 0){
+      $counter.css("color", "red")
+    } else {
+      $counter.css("color", "#545149")
+    }
+    $counter.text(finalCount)
+  })
 });
